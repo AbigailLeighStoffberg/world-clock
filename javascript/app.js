cf640be27082b6eea6b4cf2f69dc8d29f1749bb6
Abigail-Leigh Stoffberg <abizoide@gmail.com>
2025-06-25 00:41:51 +0300

    Improved css

--- javascript/app.js
+++ javascript/app.js
@@ -1,40 +1,58 @@
 
 // Local Data Display
+
+function updateLocalCity() {
 let localTimezone = moment.tz.guess();
 let localLocationElement = document.querySelector("#local-city");
 let localTimeElement = document.querySelector("#local-time");
 let localDateElement = document.querySelector("#local-date");
 
 localLocationElement.innerHTML = `${localTimezone}`;
-localTimeElement.innerHTML = `${moment().format("h:m A")}`;
+localTimeElement.innerHTML = `${moment().format("h:mm:ss A")}`;
 localDateElement.innerHTML = `${moment().format("MMMM Do YYYY")}`;
+}
 
 // Selected City Data Display
 
-function updateCity(cityTimeZone) {
+function updateSelectedCity(cityTimeZone) {
     let selectElement = document.querySelector("#city");
     cityTimeZone = selectElement.value;
-  
-    let cityName = cityTimeZone.replace("_", " ").split("/")[1];
-    let cityTime = moment().tz(cityTimeZone);
 
-    let cityLocationElement = document.querySelector("#selected-city-name");
-    let cityTimeElement = document.querySelector("#selected-city-time");
-    let cityDateElement = document.querySelector("#selected-city-date");
+    let container = document.querySelector(".selected-city-data-container");
 
-    cityLocationElement.innerHTML = cityName;
-    cityTimeElement.innerHTML = cityTime.format("h:mm:ss A");
-    cityDateElement.innerHTML = cityTime.format("MMMM Do YYYY");
+    if (cityTimeZone === "Select a City" || cityTimeZone === "") {
+        container.classList.add("no-selection");
+    } else {
+        container.classList.remove("no-selection");
+    }
+
+    if (!container.classList.contains("no-selection")) {
+      let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+      let cityTime = moment().tz(cityTimeZone);
+
+      let cityLocationElement = document.querySelector("#selected-city-name");
+      let cityTimeElement = document.querySelector("#selected-city-time");
+      let cityDateElement = document.querySelector("#selected-city-date");
+
+      cityLocationElement.innerHTML = cityName;
+      cityTimeElement.innerHTML = cityTime.format("h:mm:ss A");
+      cityDateElement.innerHTML = cityTime.format("MMMM Do YYYY");
+    }
 }
 
 let citiesSelectElement = document.querySelector("#city");
 citiesSelectElement.addEventListener("change", (event) => {
-  updateCity(event.target.value);
+  updateSelectedCity(event.target.value);
 });
 
-updateCity();
+updateSelectedCity();
 setInterval(() => {
-  updateCity();
+  updateSelectedCity();
+}, 1000);
+
+updateLocalCity();
+setInterval(() => {
+  updateLocalCity();
 }, 1000);
 
 // Fact Display
