82d97572450359118b5bb469436a510a365734d1
Abigail-Leigh Stoffberg <abizoide@gmail.com>
2025-06-24 18:44:10 +0300

    Timezone functioinality for city selected from drop down implemented

--- javascript/app.js
+++ javascript/app.js
@@ -10,70 +10,33 @@ localTimeElement.innerHTML = `${moment().format("h:m A")}`;
 localDateElement.innerHTML = `${moment().format("MMMM Do YYYY")}`;
 
 // Selected City Data Display
-function updateCity(event) {
-  let cityTimeZone = event.target.value;
-  if (cityTimeZone === "current") {
-    cityTimeZone = moment.tz.guess();
-  }
-  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
-  let cityTime = moment().tz(cityTimeZone);
-  let citiesElement = document.querySelector("#city");
-  citiesElement.innerHTML = `
-  <div class="city">
-    <div>
-      <h2>${cityName}</h2>
-      <div class="date">${cityTime.format("MMMM	Do YYYY")}</div>
-    </div>
-    <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
-    "A"
-  )}</small></div>
-  </div>
-  `;
+
+function updateCity(cityTimeZone) {
+    let selectElement = document.querySelector("#city");
+    cityTimeZone = selectElement.value;
+  
+    let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+    let cityTime = moment().tz(cityTimeZone);
+
+    let cityLocationElement = document.querySelector("#selected-city-name");
+    let cityTimeElement = document.querySelector("#selected-city-time");
+    let cityDateElement = document.querySelector("#selected-city-date");
+
+    cityLocationElement.innerHTML = cityName;
+    cityTimeElement.innerHTML = cityTime.format("h:mm:ss A");
+    cityDateElement.innerHTML = cityTime.format("MMMM Do YYYY");
 }
 
-updateTime();
-setInterval(updateTime, 1000);
-
 let citiesSelectElement = document.querySelector("#city");
-citiesSelectElement.addEventListener("change", updateCity);
+citiesSelectElement.addEventListener("change", (event) => {
+  updateCity(event.target.value);
+});
+
+updateCity();
+setInterval(() => {
+  updateCity();
+}, 1000);
 
 // Fact Display
 
 // Passport MiniGame
-
-function updateTime() {
-  // Los Angeles
-  let losAngelesElement = document.querySelector("#los-angeles");
-  if (losAngelesElement) {
-    let losAngelesDateElement = losAngelesElement.querySelector(".date");
-    let losAngelesTimeElement = losAngelesElement.querySelector(".time");
-    let losAngelesTime = moment().tz("America/Los_Angeles");
-
-    losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM	Do YYYY");
-    losAngelesTimeElement.innerHTML = losAngelesTime.format(
-      "h:mm:ss [<small>]A[</small>]"
-    );
-  }
-}
-
-function showSelectedCountryData(event) {
-        if (event.target.value.length > 0) {
-          if (event.target.value === "france") {
-            alert("Bonjour!");
-          }
-
-  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
-  let cityTime = moment().tz(cityTimeZone);
-  let citiesElement = document.querySelector("#city");
-  citiesElement.innerHTML = `
-  <div class="city">
-    <div>
-      <h2>${cityName}</h2>
-      <div class="date">${cityTime.format("MMMM	Do YYYY")}</div>
-    </div>
-    <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
-    "A"
-  )}</small></div>
-  </div>
-  `;
-}
