26e19c6197f22a301b0fc3a7b80545a890e1da16
Abigail-Leigh Stoffberg <abizoide@gmail.com>
2025-06-24 10:51:23 +0300

    Local timezone data displayed using Moment

--- javascript/app.js
+++ javascript/app.js
@@ -1,6 +1,26 @@
 
+// Local Data Display
+
 let localTimezone = moment.tz.guess();
-let localElement = document.querySelector("#local-time-zone");
-localElement.innerHTML = `Your current time zone is ${localTimezone} and the current time is ${moment().format(
-  "h:m A"
-)}`;
+let localLocationElement = document.querySelector("#local-city");
+let localTimeElement = document.querySelector("#local-time");
+let localDateElement = document.querySelector("#local-date");
+
+localLocationElement.innerHTML = `${localTimezone}`;
+localTimeElement.innerHTML = `${moment().format("h:m A")}`;
+localDateElement.innerHTML = `${moment().format("MMMM Do YYYY")}`;
+
+// Selected City Data Display
+
+let selectedLocationElement = document.querySelector("#selected-city-name");
+let selectedTimeElement = document.querySelector("#selected-city-time");
+let selectedDateElement = document.querySelector("#selected-city-date");
+
+selectedLocationElement.innerHTML = `${Timezone}`;
+selectedTimeElement.innerHTML = `${moment().format("h:m A")}`;
+selectedDateElement.innerHTML = `${moment().format("MMMM Do YYYY")}`;
+
+// Fact Display
+
+// Passport MiniGame
+
